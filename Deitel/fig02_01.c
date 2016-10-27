91d72efdabc21c8060a7f6e528812eef662270de
Stanislaw Alam <stan.alam@gmail.com>
2016-10-27 10:12:59 -0400

    updated C progs for course work

---
+++ Deitel/fig02_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ( void ) {
+
+
+	printf("Hello, Multiverse!");
+	return 0 ;
+
+
+}
---
+++ Deitel/fig02_04.c
@@ -0,0 +1,10 @@
+/* print multiple lines with a single printf */
+
+#include <stdio.h>
+
+int main ( void ) {
+
+	printf("Hello,\nMulti\nverse");
+    return 0;
+
+}
